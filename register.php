434d24486c2359b3425df3e9cb081d7202a30406
Gary Boyer <gboyer.dev@gmail.com>
2019-05-08 22:28:22 -0400

    Initial form fields created

---
+++ register.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+  <title>Welcome to slotify!</title>
+</head>
+<body>
+  <div id="inputContainer" >
+    <form id="loginForm" action="register.php" method="POST">
+      <h2>Login to your account</h2>
+      <p>
+        <label for="loginUsername">Username</label>
+        <input id="loginUsername" name="loginUsername" type="text" placeholder="username" required />
+      </p>
+      <p>
+        <label for="loginUsername">Password</label>
+        <input id="loginPassword" name="loginPassword" type="password" required />
+      </p>
+
+      <button type="submit" name="loginButton">LOG IN</button>
+
+    </form>
+
+    <form id="registerForm" action="register.php" method="POST">
+      <h2>Create a free account</h2>
+      <p>
+        <label for="username">Username</label>
+        <input id="username" name="username" type="text" placeholder="username" required />
+      </p>
+      <p>
+        <label for="firstName">First Name</label>
+        <input id="firstName" name="firstName" type="text" placeholder="First Name" required />
+      </p>
+      <p>
+        <label for="lastName">Last Name</label>
+        <input id="lastName" name="lastName" type="text" placeholder="Last Name" required />
+      </p>
+      <p>
+        <label for="email">Email</label>
+        <input id="email" name="email" type="email" placeholder="Email" required />
+      </p>
+      <p>
+        <label for="email2">Confirm</label>
+        <input id="email2" name="email2" type="email" placeholder="Confirm Email" required />
+      </p>
+      <p>
+        <label for="loginUsername">Password</label>
+        <input id="loginPassword" name="loginPassword" type="Your Password" required />
+      </p>
+      <p>
+        <label for="loginUsername">Confirm</label>
+        <input id="loginPassword" name="loginPassword" type="Confirm Password" required />
+      </p>
+
+      <button type="submit" name="registerButton">Register</button>
+
+    </form>
+  </div>
+</body>
+</html>
